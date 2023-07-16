9261551e4938ec8893276eac77746549ff558e8f
brameldering <bram.eldering@gmail.com>
2023-07-16 19:08:44 +0200

    Added modal-duration to css

--- frontend/src/components/IdeaList.js
+++ frontend/src/components/IdeaList.js
@@ -1,23 +1,23 @@
-import IdeasApi from '../services/ideasApi';
+import IdeasApi from "../services/ideasApi";
 
 class IdeaList {
   constructor() {
-    this._ideaListEl = document.querySelector('#idea-list');
+    this._ideaListEl = document.querySelector("#idea-list");
     this._ideas = [];
     this.getIdeas();
 
     this._validTags = new Set();
-    this._validTags.add('technology');
-    this._validTags.add('software');
-    this._validTags.add('business');
-    this._validTags.add('education');
-    this._validTags.add('health');
-    this._validTags.add('inventions');
+    this._validTags.add("technology");
+    this._validTags.add("software");
+    this._validTags.add("business");
+    this._validTags.add("education");
+    this._validTags.add("health");
+    this._validTags.add("inventions");
   }
 
   addEventListeners() {
-    this._ideaListEl.addEventListener('click', (e) => {
-      if (e.target.classList.contains('fa-times')) {
+    this._ideaListEl.addEventListener("click", (e) => {
+      if (e.target.classList.contains("fa-times")) {
         e.stopImmediatePropagation();
         const ideaId = e.target.parentElement.parentElement.dataset.id;
         this.deleteIdea(ideaId);
@@ -42,7 +42,7 @@ class IdeaList {
       this._ideas.filter((idea) => idea._id !== ideaId);
       this.getIdeas();
     } catch (error) {
-      alert('You can not delete this resource');
+      alert("You can not delete this resource");
     }
   }
 
@@ -53,11 +53,11 @@ class IdeaList {
 
   getTagClass(tag) {
     tag = tag.toLowerCase();
-    let tagClass = '';
+    let tagClass = "";
     if (this._validTags.has(tag)) {
       tagClass = `tag-${tag}`;
     } else {
-      tagClass = '';
+      tagClass = "";
     }
     return tagClass;
   }
@@ -67,9 +67,9 @@ class IdeaList {
       .map((idea) => {
         const tagClass = this.getTagClass(idea.tag);
         const deleteBtn =
-          idea.username === localStorage.getItem('username')
+          idea.username === localStorage.getItem("username")
             ? `<button class="delete"><i class="fas fa-times"></i></button>`
-            : '';
+            : "";
         return `
       <div class="card" data-id="${idea._id}">
      ${deleteBtn}
@@ -84,7 +84,7 @@ class IdeaList {
     </div>
       `;
       })
-      .join('');
+      .join("");
     this.addEventListeners();
   }
 }
